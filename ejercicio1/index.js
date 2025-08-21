128ffb701701f78bd01ba6a92b63bdc5f9daa05f
Danielrzzz <daniel.rodriguez2999@gmail.com>
2025-08-22 01:17:56 +0200

    update_ejercicio1

--- ejercicio1/index.js
+++ ejercicio1/index.js
@@ -1,4 +1,25 @@
-const button = document.querySelector(".showme");
-console.log(button);
-// const title = document.querySelector("#pillado");
+    //1.1
+    const button = document.querySelector(".showme");
+    console.log(button);
 
+    //1.2
+    const title = document.querySelector("#pillado");
+    console.log(title);
+
+    //1.3
+    const paragraphs = document.querySelectorAll("p");
+    console.log(paragraphs);
+
+    //1.4
+    const classes = document.querySelectorAll(".pokemon");
+    console.log(classes);
+
+    //1.5
+    const spans = document.querySelectorAll("span[data-function='testMe']");
+    console.log(spans);
+
+    //1.6
+    const characters = document.querySelectorAll("span[data-function='testMe']");
+    console.log(characters[2]);
+
+    //Los he colocado en el HTML para poder hacer las pruebas, ya que en el terminal no supe ver los elementos.
