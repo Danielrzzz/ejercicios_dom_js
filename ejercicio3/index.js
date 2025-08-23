01c78aa68a68fb9a2641fd375c982c34c45b595a
Danielrzzz <daniel.rodriguez2999@gmail.com>
2025-08-23 14:01:22 +0200

    ejercicio_3.2_cambios

--- ejercicio3/index.js
+++ ejercicio3/index.js
@@ -1,14 +1,35 @@
 //3.1
 const countries = ['Japón', 'Nicaragua', 'Suiza', 'Australia', 'Venezuela'];
-let ulList = document.createElement("ul");
-for (let i = 0; i < countries.length; i++) {
+function createLists(array) { //cree esta función cuando vi que debía hacer algo parecido en el ejercicio 3.3, para facilitarlo en caso de necesitarlo otra vez
+    let ulList = document.createElement("ul");
+    for (let i = 0; i < array.length; i++) {
     let liElement = document.createElement("li");
-    liElement.textContent = countries[i];
+    liElement.textContent = array[i];
     ulList.appendChild(liElement);
 }
-document.body.appendChild(ulList);
+return ulList;
+}
+
+document.body.appendChild(createLists(countries));
 
 
 //3.2
 let elementToDelete = document.querySelector(".fn-remove-me");
 elementToDelete.remove();
+
+
+//3.3
+const cars = ['Mazda 6', 'Ford fiesta', 'Audi A4', 'Toyota corola'];
+let addElement = document.querySelector("[data-function='printHere']")
+addElement.appendChild(createLists(cars));
+
+
+//3.4
+const countriesAgain = [ //le cambie el nombre a countriesAgain ya que el ejercicio 3.1 ya tenía el nombre countries
+	{title: 'Random title', imgUrl: 'https://picsum.photos/300/200?random=1'}, 
+	{title: 'Random title', imgUrl: 'https://picsum.photos/300/200?random=2'},
+	{title: 'Random title', imgUrl: 'https://picsum.photos/300/200?random=3'},
+	{title: 'Random title', imgUrl: 'https://picsum.photos/300/200?random=4'},
+	{title: 'Random title', imgUrl: 'https://picsum.photos/300/200?random=5'}
+];
+
